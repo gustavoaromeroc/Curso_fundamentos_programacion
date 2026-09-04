7fe4a41afeee5126cf32eab293dc6adbba8ab7ea
unknown <gustavoaromeroc@gmail.com>
2026-09-03 19:24:14 -0500

    Primer commit de los ejercicios de Python

---
+++ sintaxis.py
@@ -0,0 +1,33 @@
+# https://python.org
+
+# Comentario en una linea 
+
+"""
+Esto tambien es un 
+comentario en 
+varias lineas
+"""
+
+'''
+Esto tambien es un 
+comentario en 
+varias lineas
+'''
+my_variable = "Mi variables"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convencion 
+
+my_int = 1
+my_float = 1.1
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Otra cadena de texto'
+
+print("Hola, Python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
